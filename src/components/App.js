8c144e6554043cd7e51a31d07fccf213afee1d6a
codeviith <codeviith@Cinya>
2023-08-24 11:48:27 -0400

    React Info Flow Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
+import itemData from "../data/items"; //the second . means folder within the same folder.
+import Header from "./Header"; //the . means within the same folder.
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -11,12 +12,7 @@ function App() {
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+      <Header isDarkMode={isDarkMode} onDarkModeClick={onDarkModeClick} />
       <ShoppingList items={itemData} />
     </div>
   );
